01a49f78be7ff77782b95cba08811e005ce67707
Alexander <asaulovalex@gmail.com>
2024-09-05 08:44:54 +0300

    fixed polygons colors

--- src/utils/createPolygon.ts
+++ src/utils/createPolygon.ts
@@ -1,3 +1,4 @@
+import { DRAW_COLOR } from '../constants/mapDefaults';
 import { Action, DrawResult, DrawingActionKind, Position } from '../types';
 
 export function createPolygon(
@@ -7,10 +8,10 @@ export function createPolygon(
 ) {
   const polygon = new google.maps.Polygon({
     paths,
-    strokeColor: '#FF0000',
+    strokeColor: DRAW_COLOR,
     strokeOpacity: 0.8,
     strokeWeight: 2,
-    fillColor: '#FF0000',
+    fillColor: DRAW_COLOR,
     fillOpacity: 0.35,
   });
 
